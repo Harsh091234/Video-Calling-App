161562b4bcbd9f73ae458421549b773668f4df13
Harsh091234 <hsharma4090@gmail.com>
2026-05-17 01:56:12 +0530

    first commit

--- server/dist/modules/demo/demo.route.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const demo_controller_js_1 = require("./demo.controller.js");
-const router = (0, express_1.Router)();
-router.post("/send", demo_controller_js_1.sendName);
-exports.default = router;
